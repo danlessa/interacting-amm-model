e7d83236aa31e40277335acb96d780bc3ed3d0b0
Danilo Lessa Bernardineli <danilo.lessa@gmail.com>
2021-05-07 15:49:34 -0300

    working model

--- interacting_amm_model/main.py
+++ interacting_amm_model/main.py
@@ -1,14 +1,15 @@
 # %%
 
-from typing import NamedTuple, Dict, List
+from typing import NamedTuple, Dict, List, Optional
 from dataclasses import dataclass
 from cadCAD_tools import easy_run
 from cadCAD_tools.preparation import prepare_params, prepare_state
 import plotly.express as px
-from cadCAD_tools.types import InitialValue, Param, Signal, StateUpdate, VariableUpdate
+from cadCAD_tools.types import InitialValue, Param, Signal, StateUpdate, VariableUpdate, ParamSweep
 from enum import Enum
 from random import random, choice
 import pandas as pd
+import numpy as np
 
 Wei = int
 Percentage = float
@@ -33,6 +34,11 @@ class PairState():
             self.reserve_token_1 += o.reserve_token_1
             self.reserve_token_2 += o.reserve_token_2
         return self
+    
+    def __mul__(self, o: float):
+        self.reserve_token_1 *= o
+        self.reserve_token_2 *= o
+        return self
 
 
 class AMM_States(dict):
@@ -54,7 +60,7 @@ amms = [AMM('uniswap', 0.003, 20),
         AMM('curve', 0.0015, 10)]
 
 initial_reserve_1 = 1000
-initial_reserve_2 = 1000
+initial_reserve_2 = 5000
 
 pair_states = {amm.label: PairState(initial_reserve_1, initial_reserve_2)
                for amm in amms}
@@ -63,81 +69,109 @@ pair_states = AMM_States(pair_states)
 
 initial_state = {
     'market_price_token_1': InitialValue(5.0, Fiat),
-    'market_price_token_2': InitialValue(5.0, Fiat),
     'pair_state': InitialValue(pair_states, AMM_States)
 }
 
 params = {
-    'amms': Param(amms, list[AMM])
+    'amms': Param(amms, list[AMM]),
+    'user_action_intensity': Param(0.1, Percentage),
+    'arbitrage_intensity': ParamSweep([0.1, 0.9], Percentage),
+    'swap_vs_liquidity_preference': Param(0.9, Percentage)
 }
 
 
 def p_arbitrage(params, _2, _3, state) -> Signal:
+    """
+    Arbitrage on each individual AMM so that it rebound to the market price.
+    """
+    # Retrieve state and params
+    arbitrage_intensity = params['arbitrage_intensity']
     pair_states: AMM_States = state['pair_state']
+    market_price = state['market_price']
+
     delta_pair_state = AMM_States()
-    for amm_1, pair_state_1 in pair_states.items():
-        for amm_2, pair_state_2 in pair_states.items():
+    for amm, pair_state in pair_states.items():
+        # AMM State
+        reserve_1 = pair_state.reserve_token_1
+        reserve_2 = pair_state.reserve_token_2
+        amm_price = reserve_2 / reserve_1
 
-            reserve_token_1_amm_1 = 0
-            reserve_token_1_amm_2 = 0
-            price_token_1 = 0
-            price_token_1_amm_1 = 0
-            price_token_1_amm_2 = 0
+        # Optimal Token 1 value to make the AMM rebound to the market price
+        optimal_value = (reserve_1 - reserve_2) / (amm_price + market_price)
 
-            price_token_2 = 0
-            price_token_2_amm_1 = 0
-            price_token_2_amm_2 = 0
+        # Optimal User Action for making the rebound
+        optimal_arbitrage = PairState(-optimal_value, optimal_value * amm_price)
 
-            delta_pair_state[amm_1] = amm_1_arbitrage
-            delta_pair_state[amm_2] = amm_2_arbitrage
+        # Randomize the intensity
+        real_arbitrage = optimal_arbitrage * random() * arbitrage_intensity
 
-
-    
+        # Act on it
+        delta_pair_state[amm] = real_arbitrage
 
     return {'delta_pair_state': delta_pair_state}
 
 
 def p_user_action(params, _2, _3, state) -> Signal:
+    """
+    Take random user actions
+    """
+    # Retrieve state and params
+    user_action_intensity = params['user_action_intensity']
+    swap_vs_liquidity_preference = params['swap_vs_liquidity_preference']
     pair_states: AMM_States = state['pair_state']
+
     delta_pair_state = AMM_States()
     for amm, pair_state in pair_states.items():
-
+        # AMM State
         token_1_reserve = pair_state.reserve_token_1
         token_2_reserve = pair_state.reserve_token_2
 
-        action: str = choice(UserAction)
+        # Decide if the action is going to be swap or mint/burn
+        if random() < swap_vs_liquidity_preference:
+            action = 'Swap'
+        else:
+            action = 'MintBurn'
+
+        # Decide the direction of the swap or mint / burn
         direction: int = choice(UserActionDirection)
 
-        magnitude: float = random() * 0.3
+        # Intensity in terms of available reserve
+        intensity: float = random() * user_action_intensity
+
+        # Set the token amounts based on the sorted direction
         if direction == 1:
-            token_amount_1 = token_1_reserve * magnitude
+            token_amount_1 = token_1_reserve * intensity
             token_amount_2 = token_amount_1 * token_2_reserve / token_1_reserve
         else:
-            token_amount_2 = token_2_reserve * magnitude
+            token_amount_2 = token_2_reserve * intensity
             token_amount_1 = token_amount_2 * token_1_reserve / token_2_reserve
 
-        if action is 'Swap':
+        # Take action
+        if action == 'Swap':
             token_1_reserve = -1 * token_amount_1 * direction
             token_2_reserve = token_amount_2 * direction
-
-        elif action is 'MintBurn':
+        elif action == 'MintBurn':
             token_1_reserve = token_amount_1 * direction
             token_2_reserve = token_amount_2 * direction
         else:
             token_1_reserve = 0
             token_2_reserve = 0
-
+        
+        # Append the AMM action
         delta_pair_state[amm] = PairState(token_1_reserve, token_2_reserve)
     return {'delta_pair_state': delta_pair_state}
 
 
-def s_pair_state(params, _2, _3, state, signal) -> VariableUpdate:
+def s_pair_state(_1, _2, _3, state, signal) -> VariableUpdate:
+    # State and signals
     new_pair_state: AMM_States = state['pair_state']
     delta_pair_state: AMM_States = signal['delta_pair_state']
+
     # Sum the change on the pair states
     for amm_label, amm_pair_state in delta_pair_state.items():
         new_pair_state[amm_label] += amm_pair_state
 
+    # Assign new variable
     return ('pair_state', new_pair_state)
 
 
@@ -152,15 +186,34 @@ def brownian_motion(variable) -> StateUpdate:
     return suf
 
 
+def p_market_price(_1, _2, _3, state) -> Signal:
+    """
+    Retrieve the market price as proxied by the mean value of the AMMs
+    as well as its associated volatility
+    """
+    pair_states: AMM_States = state['pair_state']
+    prices = []
+    for _, pair_state in pair_states.items():
+        amm_price = pair_state.reserve_token_2 / pair_state.reserve_token_1
+        prices.append(amm_price)
+    return {'market_price': np.mean(prices),
+            'market_price_volatility': np.std(prices)}
+
+def generic_suf(variable) -> StateUpdate:
+    def suf(params, substep, history, state, signal):
+        return (variable, signal[variable])
+    return suf
+
 timestep_block = [
     {
         'label': 'Market Movements',
         'policies': {
+            'market_price': p_market_price
 
         },
         'variables': {
-            'market_price_token_1': brownian_motion('market_price_token_1'),
-            'market_price_token_2': brownian_motion('market_price_token_2')
+            'market_price': generic_suf('market_price'),
+            'market_price_volatility': generic_suf('market_price_volatility'),
         }
     },
     {
@@ -178,7 +231,7 @@ timestep_block = [
 ]
 
 N_timesteps = 100
-N_samples = 3
+N_samples = 5
 initial_state = prepare_state(initial_state)
 params = prepare_params(params)
 
@@ -197,8 +250,16 @@ results.market_price_token_1
 # %%
 px.line(results,
         x='timestep',
-        y=['market_price_token_1', 'market_price_token_2'],
-        facet_col='run',
+        y='market_price',
+        facet_col='subset',
+        line_group='run')
+
+
+# %%
+px.line(results,
+        x='timestep',
+        y='market_price_volatility',
+        facet_col='subset',
         line_group='run')
 # %%
 print(results.pair_state.iloc[-1])
@@ -220,7 +281,7 @@ y_cols = [f"{amm.label}_reserve_token_1" for amm in amms]
 px.line(results,
         x='timestep',
         y=y_cols,
-        facet_col='run',
+        facet_col='subset',
         line_group='run')
 
 # %%
